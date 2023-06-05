7154d78ce20fc7d526e5467bb451eedd2fb55b9a
Diego Costa <diegocosta@unip.br>
2023-06-04 22:27:52 -0300

    ajustes

--- Formula1-Angular/src/app/core/core.module.ts
+++ Formula1-Angular/src/app/core/core.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 
@@ -11,7 +14,10 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   exports: [
     FooterComponent,
