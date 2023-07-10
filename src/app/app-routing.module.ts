aa1fe8a7cac6c52c1796cc9db16ea861c96cdd0d
Diego Costa <diegocosta@unip.br>
2023-07-10 10:07:18 -0300

    colocando titulo nas views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'home', loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule) },
-  {path: 'teams', loadChildren: () => import('./views/teams/teams.module').then(m => m.TeamsModule) },
-  {path: 'latest', loadChildren: () => import('./views/latest/latest.module').then(m => m.LatestModule) },
-  {path: 'results', loadChildren: () => import('./views/results/results.module').then(m => m.ResultsModule) },
-  {path: 'drivers', loadChildren: () => import('./views/drivers/drivers.module').then(m => m.DriversModule) },
-  {path: 'schedule', loadChildren: () => import('./views/schedule/schedule.module').then(m => m.ScheduleModule) },
-  {path: 'not-found', loadChildren: () => import('./views/not-found/not-found.module').then(m => m.NotFoundModule) },
+  {path: 'home', title: 'Formula 1 - Angular', loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule) },
+  {path: 'teams', title: 'Teams', loadChildren: () => import('./views/teams/teams.module').then(m => m.TeamsModule) },
+  {path: 'latest', title: 'Latest', loadChildren: () => import('./views/latest/latest.module').then(m => m.LatestModule) },
+  {path: 'results', title: 'Results', loadChildren: () => import('./views/results/results.module').then(m => m.ResultsModule) },
+  {path: 'drivers', title: 'Drivers', loadChildren: () => import('./views/drivers/drivers.module').then(m => m.DriversModule) },
+  {path: 'schedule', title: 'Schedule', loadChildren: () => import('./views/schedule/schedule.module').then(m => m.ScheduleModule) },
+  {path: 'not-found', title: 'Not Found', loadChildren: () => import('./views/not-found/not-found.module').then(m => m.NotFoundModule) },
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: '**', redirectTo: 'not-found', pathMatch: 'full'},
 ];
