2a74e664a97ddaca5af113bbdf818040dd5e8785
Diego Costa <dit43@live.com>
2024-07-04 18:50:44 -0300

    adicionando timeline da F1 no twitter embedado

--- src/app/views/latest/latest.component.ts
+++ src/app/views/latest/latest.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./latest.component.scss']
 })
 export class LatestComponent {
-
+  ngAfterViewInit(): void {
+    (<any>window).twttr.widgets.load();
+}
 }
