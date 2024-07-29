cefc9fd50f273c239c0e35c9d825a56c4bf7278d
Diego Costa <dit43@live.com>
2024-07-29 18:46:43 -0300

    requisição de construtores completa

--- src/app/models/tables/constructors-table.ts
+++ src/app/models/tables/constructors-table.ts
@@ -1,8 +1,6 @@
 import { Constructor } from "../constructor";
 import { BaseTable } from "./base-table";
 
-
-
 export interface ConstructorsTable extends BaseTable { 
     season: string;
     Constructors: Constructor []
--- src/app/views/teams/teams.module.ts
+++ src/app/views/teams/teams.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TeamsRoutingModule } from './teams-routing.module';
+import { TeamsComponent } from './teams.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TeamsComponent
+  ],
   imports: [
     CommonModule,
     TeamsRoutingModule
