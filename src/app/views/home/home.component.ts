683a802d0a0383cdf8bd79126aa38ebff887dcfb
Diego Costa <diegocosta@unip.br>
2024-08-29 12:51:48 -0300

    adicionando @if e @for nas paginas que usavam a versão antiga

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoaderService } from 'src/app/services/loader-service.service';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  
+  constructor (public loaderService: LoaderService){}
   
  }
