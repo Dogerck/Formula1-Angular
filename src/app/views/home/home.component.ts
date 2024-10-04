bfbb189b7ce1bbef1e981380dd6abe03bafa2f3f
Diego Costa <diegocosta@unip.br>
2024-10-04 11:11:02 -0300

    ajuste no loader do iframe e adicionandi a condicional @empty caso o @for não retorne nada

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, Renderer2, ViewChild } from '@angular/core';
 import { LoaderService } from 'src/app/services/loader-service.service';
 
 @Component({
@@ -6,7 +6,20 @@ import { LoaderService } from 'src/app/services/loader-service.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('iframe', { static: false }) iframe!: ElementRef<HTMLIFrameElement>;
   constructor (public loaderService: LoaderService){}
-  
+  private renderer = inject(Renderer2);
+  loading: boolean = true;
+
+
+  ngAfterViewInit(): void {
+    this.renderer.setStyle(this.iframe.nativeElement, 'display', 'none');
+    this.iframe.nativeElement.onload = () => {
+      this.loading = false;
+      this.renderer.setStyle(this.iframe.nativeElement, 'display', 'block');
+    };   
+    console.log(this.iframe.nativeElement);
+    
+}
  }
